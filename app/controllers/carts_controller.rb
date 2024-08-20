d5912c0e3b97a190d347666396148c786cc38606
santino doucet <doucetsantino@gmail.com>
2024-08-20 17:04:24 +0200

    route configuration ans action controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,19 @@
 class CartsController < ApplicationController
-  def index
-  end
 
   def show
+    
   end
 
   def edit
+
   end
 
   def update
+
   end
+
+  def send_order_to_merchant
+
+  end
+
 end
