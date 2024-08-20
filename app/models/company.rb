82c70840dccfc54a395243fa8dcab65864ee6aac
kevin rossi <kevinrossi@free.fr>
2024-08-20 11:01:52 +0200

    reviewed validations and associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :user
-  has_many :items, :orders
+  has_many :items, dependent: :destroy
+  has_many :orders, dependent: :destroy
   validates :siren, :name, presence: true
+  validates :siren, uniqueness: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,4 @@ class Item < ApplicationRecord
 
   validates :reference, :brand, :model, :color, :price, :size, presence: true
   validates :price, comparison: { greater_than_or_equal_to: 1 }
-  validates :reference, uniqueness: { scope: [:color, :size], message: "Combination with color and size must be unique, combination already exists" }
 end
