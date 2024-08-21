8c76717d0581482f1aed32889851c23f2b3a9f80
kevin rossi <kevinrossi@free.fr>
2024-08-21 12:01:52 +0200

    Added item controller show logic

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all
     if params[:query].present?
       @items = @items.search_by_brand_model_reference_and_color(params[:query])
     end
+  end
+
+  def show
   end
 
   def new
@@ -22,6 +26,13 @@ class ItemsController < ApplicationController
 
   end
 
-  def show
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:reference, :brand, :model, :color, :price, :size)
   end
 end
