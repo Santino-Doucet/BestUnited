0037f6b60c59cb6c183b1086cba8dfa69eb03a47
JiFang Lo <jennylo2477@gmail.com>
2024-08-21 14:35:38 +0200

    search bar fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.all
     if params[:query].present?
