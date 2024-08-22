f29427d690840031785836375b0457d2b40e3d3b
kevin rossi <kevinrossi@free.fr>
2024-08-22 15:14:11 +0200

    Added search by city and city+model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,24 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.with_attached_photo.all
-    if params[:search][:query].present?
-      @items = @items.search_by_brand_model_reference_and_color(params[:search][:query])
+    if params[:search][:address].present? && params[:search][:query].present?
+      temp_items = @items.search_by_brand_model_reference_and_color(params[:search][:query]).with_attached_photo
+      comp_items = []
+      Company.near(params[:search][:address], 10).each do |company|
+        company.items.each do |item|
+          comp_items << item
+        end
+      end
+      @items = temp_items & comp_items
+    elsif params[:search][:query].present?
+      @items = @items.search_by_brand_model_reference_and_color(params[:search][:query]).with_attached_photo
+    elsif params[:search][:address].present?
+      @items = []
+      Company.near(params[:search][:address], 10).each do |company|
+        company.items.each do |item|
+          @items << item
+        end
+      end
     end
   end
 
