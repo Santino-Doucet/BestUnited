35ceebcac9986ac1f90686778bdadeb542df34a3
amandine porte <porteamandine@hotmail.fr>
2024-08-23 11:20:39 +0200

    add chart pie and customize page dash

--- app/javascript/switch-tabs.js
+++ app/javascript/switch-tabs.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const tabs = document.querySelectorAll('.tab-page-index');
-  const sections = document.querySelectorAll('.card-section-page-index');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const tabs = document.querySelectorAll('.tab-page-index');
+//   const sections = document.querySelectorAll('.card-section-page-index');
 
-  tabs.forEach(tab => {
-    tab.addEventListener('click', function() {
-      // Remove active class from all tabs
-      tabs.forEach(t => t.classList.remove('active'));
+//   tabs.forEach(tab => {
+//     tab.addEventListener('click', function() {
+//       // Remove active class from all tabs
+//       tabs.forEach(t => t.classList.remove('active'));
 
-      // Add active class to clicked tab
-      this.classList.add('active');
+//       // Add active class to clicked tab
+//       this.classList.add('active');
 
-      // Hide all sections
-      sections.forEach(section => section.classList.remove('active'));
+//       // Hide all sections
+//       sections.forEach(section => section.classList.remove('active'));
 
-      // Show the target section
-      const target = this.getAttribute('data-target');
-      document.getElementById(target).classList.add('active');
-    });
-  });
+//       // Show the target section
+//       const target = this.getAttribute('data-target');
+//       document.getElementById(target).classList.add('active');
+//     });
+//   });
 
-  // Trigger a click on the active tab to show the corresponding section
-  document.querySelector('.tab-page-index.active').click();
-});
+//   // Trigger a click on the active tab to show the corresponding section
+//   document.querySelector('.tab-page-index.active').click();
+// });
