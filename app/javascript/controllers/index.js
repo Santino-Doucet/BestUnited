7d518d84984f9202cfb55a9007cbc0d539875948
JiFang Lo <jennylo2477@gmail.com>
2024-08-23 11:37:00 +0200

    pages index js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
 import SwiperController from "../controllers/swiper_controller"
 application.register("swiper", SwiperController)
+
+import TabsController from "../controllers/tabs_controller"
+application.register("tabs", TabsController)
