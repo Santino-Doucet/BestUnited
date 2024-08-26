97eb2f86854c2ac65c760610e07b00c5dfb0f44e
kevin rossi <kevinrossi@free.fr>
2024-08-26 13:55:28 +0200

    add complete inputs in items index

--- app/javascript/controllers/reset_search_controller.js
+++ app/javascript/controllers/reset_search_controller.js
@@ -3,11 +3,33 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="reset-search"
 export default class extends Controller {
 
-  static targets = ["search", "find"]
-  connect() {
+  static targets = ["search", "address"]
+
+  sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
 
-  clearSearch() {
-    this.searchTarget.value = ""; // Clear the input field
+  connect() {
+    this.searchTarget.value = ""
+    this.setInputValueFromUrl()
   }
+
+  async setInputValueFromUrl() {
+    // Get the URL parameters
+    const params = new URLSearchParams(window.location.search)
+    // Retrieve the specific parameter (e.g., "query")
+    const searchQuery = params.get('search[query]');
+    const searchAddress = params.get('search[address]');
+
+    // Set the value of the input field if parameter is present
+    if (searchQuery) {
+      this.searchTarget.value = searchQuery;
+    }
+
+    if (searchAddress) {
+      await this.sleep(1000)
+      this.addressTarget.value = searchAddress;
+    }
+  }
+
 }
