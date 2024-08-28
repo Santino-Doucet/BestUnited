3d59e991ddde24fc407bac8240c415c441e6d5b5
kevin rossi <kevinrossi@free.fr>
2024-08-28 10:30:52 +0200

    removed duplicate in items index

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,27 +5,39 @@ class Item < ApplicationRecord
   validates :brand, :model, :color, :price, :size, presence: true
   validates :price, comparison: { greater_than_or_equal_to: 1 }
   validate :barcode_or_reference_present
+
   has_many :order_items
   has_many :orders, through: :order_items
 
   scope :not_in_stock, -> { joins(:orders).where(orders: { status: ["En attente", "Validée", "Refusée", "Effectuée"] }) }
   scope :in_stock, -> { where.not(id: not_in_stock) }
 
+  # Define the scope to find items with the same company, model, brand, price, and color
+  scope :similar_items, ->(item) {
+    where(
+      company_id: item.company_id,
+      model: item.model,
+      brand: item.brand,
+      price: item.price,
+      color: item.color
+    ).where.not(id: item.id) # Exclude the current item if needed
+  }
+
   include PgSearch::Model
   pg_search_scope :search_by_brand_model_reference_and_color,
-    against: [ :brand, :model, :reference, :color ],
-    using: {
-      tsearch: { prefix: true }
-    }
+                  against: [:brand, :model, :reference, :color],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
   def same_in_stock
     company.items.where(
-      reference:,
-      brand:,
-      model:,
-      color:,
-      price:,
-      size:
+      reference: reference,
+      brand: brand,
+      model: model,
+      color: color,
+      price: price,
+      size: size
     )
   end
 
@@ -33,7 +45,7 @@ class Item < ApplicationRecord
     unique_items = {}
 
     items.each do |item|
-      key = [item.reference, item.brand, item.model, item.color, item.price, item.size]
+      key = [item.reference, item.brand, item.model, item.color, item.price]
 
       if unique_items[key]
         unique_items[key][:count] += 1
