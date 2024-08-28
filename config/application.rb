a7f0061ede82ee363fb35fa011d19590d0ff3749
JiFang Lo <jennylo2477@gmail.com>
2024-08-28 09:53:45 +0200

    pop up message to fr

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BestUnited
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :fr
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
